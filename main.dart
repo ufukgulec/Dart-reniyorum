bfb9f920706725f0bb2d60aa42b342ddcdc395cf
Ufuk <51711890+ufukgulec@users.noreply.github.com>
2022-08-10 19:39:42 +0000

    Degiskenler

--- main.dart
+++ main.dart
@@ -1,15 +1,22 @@
 void main() {
-  String Username="Ufukgulec";
+    String Username = "Ufukgulec";
   print(Username);
 
-  int a=10;
-  int b=20;
+  int a = 10;
+  int b = 20;
 
-  int c=a+b;
+  int c = a + b;
 
-  print("a+b = "+c.toString());
-  
+  print("a+b = " + c.toString());
+
   double myDoubleNum = 5.99;
 
   bool myBool = true;
+
+  print(true && false);
+  print(false || true);
+  print(!true);
+
+  var list=[1,2,3,4];
+  print(list);
 }
